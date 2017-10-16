1ca022acc6accd3122cb9bd09dccf22de4ddf2f9
kseans8 <kseans8@gmail.com>
2017-10-16 15:16:56 -0400

    strengthend tests and updated super cool ui

--- spec/features/answer_a_question_spec.rb
+++ spec/features/answer_a_question_spec.rb
@@ -6,9 +6,20 @@ feature "Answering a question" do
 
     visit(question_path(question))
 
+    click_link('Answer this Question')
     fill_in('Answer', :with => "I'm here for you my dude.")
-    click_button('Submit')
+    click_button('Submit Answer')
 
     expect(page).to have_content("I'm here for you my dude.")
+  end
+  scenario 'I go to a question, and I try to be a jerk and submit a blank string' do
+    question = Question.create(title: 'Can you not be a jerk?')
+
+    visit(question_path(question))
+
+    click_link('Answer this Question')
+    click_button('Submit Answer')
+
+    expect(page).to have_content("Answer can't be blank")
   end
 end
